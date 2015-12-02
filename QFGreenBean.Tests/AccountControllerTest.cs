b69d302226cbd7c71815df0d04c183402cfe0ecb
layheng <henglay@hotmail.com>
2015-12-02 15:20:06 -0500

    Add unit test for Account Controller.

---
+++ QFGreenBean.Tests/AccountControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QFGreenBean.Controllers;
+
+namespace QFGreenBean.Tests
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            AccountController controller = new AccountController();
+
+            Assert.IsNotNull(controller.Login("john"));
+
+        }
+    }
+}
