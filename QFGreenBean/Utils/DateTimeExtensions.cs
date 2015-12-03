fa306e9bb34e27cc4824882accf9a0f8cff21ae1
layheng <henglay@hotmail.com>
2015-12-03 01:16:52 -0500

    Add DATETIME extension utility.

---
+++ QFGreenBean/Utils/DateTimeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace QFGreenBean.Utils
+{
+    public  static class DateTimeExtensions
+    {
+        // Convert old DATETIME object to equivalent Day and Time of the week today
+        public static DateTime EqualTodayWeekDayTime(this DateTime oldDate)
+        {
+            int num1 = (int)oldDate.DayOfWeek;
+            int num2 = (int)DateTime.Today.DayOfWeek;
+            DateTime result = DateTime.Today.AddDays(num1 - num2);
+
+            return (result + oldDate.TimeOfDay);
+        }
+    }
+
+    // Code Example:
+    // DateTime dtOld = new DateTime(2015, 09, 11, 11, 30, 00);
+    // DateTime result = dtOld.EqualTodayWeekDayTime();
+    // Console.WriteLine(result.ToString());
+    // Console.WriteLine(result.ToString("ddd"));
+}
